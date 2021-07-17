805064d181749ff371e7c74f4da10a9284f92a44
Joe Rocks <joe@joerocks.net>
2021-07-16 21:57:42 -0700

    Simply start

--- src/server/PointsService.server.lua
+++
@@ -1,6 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Knit)
-
-local PointsService = require(ReplicatedStorage.Common.PointsService)
-
-Knit.Start():Catch(warn)
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,6 @@
-print("Hello world, from server!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Knit = require(ReplicatedStorage.Knit)
+
+local PointsService = require(ReplicatedStorage.Common.PointsService)
+
+Knit.Start():Catch(warn)
