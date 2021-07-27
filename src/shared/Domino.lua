9c8a2e826307c54129b4089fff16e120347f265f
Joe Rocks <joe@joerocks.net>
2021-07-26 20:00:02 -0700

    Add Domino functions

--- src/shared/Domino.lua
+++ src/shared/Domino.lua
@@ -1,9 +1,28 @@
 local Domino = {}
 
-function Domino:new(firstPip,secondPip)
-    self.isDouble = firstPip == secondPip
-    self.firstPip = firstPip
-    self.secondPip = secondPip
+function Domino:new(pip1,pip2)
+    self.isDouble = pip1 == pip2
+    self.isPointingInward = false
+    self.pip1 = pip1
+    self.pip2 = pip2
+end
+
+function Domino:getOutwardValue()
+    if self.isDouble then
+        return self.pip1 * 2
+    elseif self.isPointingInward then
+        return self.pip1
+    else
+        return self.pip2
+    end
+end
+
+function Domino:getOutwardPipValue()
+    if self.isDouble or self.isPointingInward then
+        return self.pip1
+    else
+        return self.pip2
+    end
 end
 
 return Domino
